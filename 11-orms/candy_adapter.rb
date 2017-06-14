7d5f5b956e6229cc7f10a5f9af83b652ab3478ae
Jeff Katz <jeffreyerickatz@gmail.com>
2017-06-14 16:32:26 -0400

    orm start

--- 11-orms/candy_adapter.rb
+++ 11-orms/candy_adapter.rb
@@ -1,46 +1,56 @@
 require 'sqlite3'
-def db
-  @db ||= SQLite3::Database.new("candy.db")
-  # @db || @db = SQLite3::Database.new("candy.db")
-  # @sum += 1
-  # @sum = @sum + 1
-  # if @db
-  #   @db
-  # else
-  #  @db = SQLite3::Database.new("candy.db")
-  # end
+require 'pry'
+
+class Candy
+  attr_accessor :name, :calories, :has_milk
+  def self.db
+    @@db = SQLite3::Database.new("candy.db")
+  end
+
+
+  def insert
+    db.execute("INSERT INTO candies (name, calories, has_milk) VALUES ('snickers', 50, 1);")
+  end
 end
+
+Pry.start
+
+# candy = Candy.new
+# candy.name = 'snickers'
+# candy.calories = 80
+# candy.has_milk = true
+# candy.insert
 #
-# db.execute()
-# create
-
-def create(name, calories, has_milk)
-  db.execute("INSERT INTO candies (name, calories, has_milk) VALUES ('snickers', 50, 1);")
-end
-
-
-# index
-
-def index
-  db.execute("select name from candies;")
-end
-
-
-# show
-
-def show(id)
-  #
-end
- # show one candy, by the id
-
-# update the candy
-def update(id, attributes)
-  # attributes = {name: 'chocolate and peanuts', calories: 10}
-end
-
-# destroy
-  # destroy candy by the id
-
-def destroy(id)
-
-end
+#
+# #
+# # db.execute()
+# # create
+#
+#
+#
+#
+# # index
+#
+# def index
+#   db.execute("select name from candies;")
+# end
+#
+#
+# # show
+#
+# def show(id)
+#   #
+# end
+#  # show one candy, by the id
+#
+# # update the candy
+# def update(id, attributes)
+#   # attributes = {name: 'chocolate and peanuts', calories: 10}
+# end
+#
+# # destroy
+#   # destroy candy by the id
+#
+# def destroy(id)
+#
+# end
