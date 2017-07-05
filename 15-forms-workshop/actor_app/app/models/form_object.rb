35fea267b4fda6221201d9a084ad2b19562465d6
Flatiron School <flatironschool@Flatirons-MacBook-Pro.local>
2017-07-05 15:02:06 -0400

    adding code

---
+++ 15-forms-workshop/actor_app/app/models/form_object.rb
@@ -0,0 +1,19 @@
+class FormObject
+  attr_reader :object
+  def initialize(activerecord_instance)
+    @object = activerecord_instance
+  end
+
+  def my_text_field(name_attribute)
+    # we want it to break if there is no attribute/method
+    html = <<-HTML
+      <input type="text" name="actor[name]" id="actor_name" />
+    HTML
+    html.html_safe
+  end
+end
+
+
+# f = FormObject.new(@actor)
+# f.text_field :name
+# <input type="text" name="#{name_attribute}">
