3c5e62b8cddbe36d1cc1e3701d3d4424d6bb8c84
Johann Kerr <johannkerr@gmail.com>
2017-07-10 16:51:45 -0400

    added auth lecture-code"

---
+++ 16-auth/toystore/app/controllers/application_controller.rb
@@ -0,0 +1,28 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    else
+    end
+  end
+
+
+  def logged_in?
+    !!current_user
+  end
+
+
+  def authorized
+    if logged_in?
+
+    else
+      redirect_to login_path
+    end
+  end
+
+end
