eea0548310fcbb90f4ffda12ca4e75569efcda26
Flatiron School <flatironschool@Flatirons-MacBook-Pro.local>
2017-07-21 09:10:26 -0400

    adding object relations code

---
+++ jquery/object-relations.js
@@ -0,0 +1,20 @@
+let store = {teachers: [{}, {}], courses: []}
+
+//
+//
+class Teacher {
+
+}
+
+class Course {
+
+}
+
+
+avi = new Teacher('avi')
+// {id: 1, name: 'avi'}
+course = new Course('ruby', avi)
+// {name: 'ruby', teacher_id: 1}
+course.teacher()
+teacher.courses()
+// single source of truth
--- jquery/samples.js
+++ jquery/samples.js
@@ -101,27 +101,6 @@ $(document).ready(function(){
 
 
 
-let store = {teachers: [{}, {}], courses: []}
-
-//
-//
-class Teacher {
-
-}
-
-class Course {
-
-}
-
-
-avi = new Teacher('avi')
-// {id: 1, name: 'avi'}
-course = new Course('ruby', avi)
-// {name: 'ruby', teacher_id: 1}
-course.teacher()
-teacher.courses()
-// single source of truth
-
 
 
 
