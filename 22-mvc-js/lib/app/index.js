f89fa0ad9671e424c57c5a427a1a3bc0959c2dac
Flatiron School <flatironschool@Flatirons-MacBook-Pro.local>
2017-07-21 13:38:43 -0400

    adding mvc js code

---
+++ 22-mvc-js/lib/app/index.js
@@ -0,0 +1,16 @@
+
+// When we type in a teachers name, and hometown, and then click submit we want to:
+// ...
+
+
+
+let store = {teachers: []}
+$(function(){
+
+})
+
+class Teacher {
+  constructor(name, hometown){
+
+  }
+}
