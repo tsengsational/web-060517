4c4591753add10a99c02f3f0cba8cba59e80a7bd
github username <github email address>
2017-07-27 11:27:22 -0400

    stuff

--- 22-mvc-js/lib/app/index.js
+++ 22-mvc-js/lib/app/index.js
@@ -6,6 +6,29 @@
 
 let store = {teachers: []}
 
-class Teacher {
+function createTeacher(){
+  let idCounter = 0
 
+ return class Teacher {
+    constructor(name, hometown){
+      this.name = name
+      this.hometown = hometown
+      this.id = ++idCounter
+      store.teachers.push(this)
+    }
+  }
 }
+
+let teacher = createTeacher()
+
+$(document).ready(function(){
+  $('form').on('submit', function(event){
+    let name = $('#name').val();
+    let hometown = $('#hometown').val();
+    let newTeacher = new teacher(name, hometown)
+
+   $('ul').append(`<li>name: ${newTeacher.name} hometown: ${newTeacher.hometown}</li>`)
+
+   event.preventDefault();
+  })
+})
